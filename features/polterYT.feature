932a3223e2740d19de86b4414de8a5b36f2ab578
Hector <hector5991@gmail.com>
2019-04-23 18:50:38 +0200

    Travis up

--- features/polterYT.feature
+++ features/polterYT.feature
@@ -1,6 +1,33 @@
-Feature:
-
-  Scenario: View home page
-    Given I am on the home page
-    When I press space
-    Then I should see "INICIAR SESIÓN"
+Feature: Is an agile video?
+  
+  Everybody wants to be agile
+    
+  Scenario: Search for Videos of Large Rodents
+    Given I am on the YouTube page
+    When I search for "capybara"
+    Then videos of large rodents are returned
+    
+  Scenario: Agile found 
+      Given I am on the YouTube page
+      When I search for "Agile"
+      Then Agile video is "found"
+      
+  Scenario: Agile not found, RUP is not Agile
+      Given I am on the YouTube page
+      When I search for "RUP"
+      Then Agile video is "not found"
+      
+  Scenario: Agile Watch video
+      Given I searched for an agile video
+      When I click on it
+      Then It starts to play
+      
+  Scenario: Share agile video
+      Given I am watching an agile video
+      When I click on the share button
+      Then Share options are shown
+      
+  Scenario: Agile recommendations
+      Given I am watching an agile video
+      When I click on the configuration button
+      Then Configuration properties are shown
--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -35,16 +35,81 @@ Then("devuelvo trenes") do
   page.should have_content 'A'
 end
 
-Given(/^I am on the home page$/) do
-  puts  visit "https://www.youtube.com/watch?v=NoxF1VpdK9I"
+require 'capybara/dsl'
+include Capybara::DSL
+Given(/^I am on the YouTube page$/) do
+  visit ('https://www.youtube.com')
 end
 
-When /^I press space$/ do
-  click_button 'Botón de reproducción (k)'
+Given(/^I am on the YouTube home page$/) do
+  pending # express the regexp above with the code you wish you had
 end
 
-Then(/^I should see "(.*?)"$/) do |text|
-  page.should have_content text
-
-
+When(/^I search for "(.*?)"$/) do |search|
+  fill_in 'search_query', :with => search
+  puts page.inspect
+  #find_button('search-icon-legacy').trigger('click')
+  click_on 'Search'
 end
+
+Then("videos of large rodents are returned") do
+    puts page.inspect
+    page.has_content?('Animal')
+end
+
+
+Then(/^Agile video is "([^"]*)"$/) do |isAgile|
+    
+    puts page.inspect
+    if (page.has_content?("Agile"))
+        
+            found = 'found'
+    else
+    
+            found = 'not found'
+    end
+    found == isAgile
+end
+
+Given("I searched for an agile video") do
+    visit ('https://www.youtube.com/results?search_query=agile')
+end
+
+
+
+When("I click on it") do
+     click_on 'What is Agile?'
+    
+end
+
+
+Then("It starts to play") do 
+    puts page.inspect
+    page.find_by_id('movie_player')
+end
+
+Given("I am watching an agile video") do
+    visit('https://www.youtube.com/watch?v=nGw23NDzRrg&t=2s')
+end
+
+
+When("I click on the share button") do
+    find('button[aria-label=Share]').trigger('click')
+end
+
+Then("Share options are shown") do
+    puts page.inspect
+    page.has_content?('Copyr')
+end
+
+When("I click on the configuration button") do
+   find('button[aria-label=Settings]').trigger('click') 
+end
+
+Then("Configuration properties are shown") do
+    puts page.inspect
+    find('button[aria-expanded=true]')
+end
+
+
+
